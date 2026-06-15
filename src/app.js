54c3d122731f74ef7221447d93cf641b9adcafe9
vdiazBI <xavier.diaz.p@gmail.com>
2026-06-15 14:25:25 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.get('/greet', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    return res.status(400).json({ error: 'name query param is required' });
+  }
+  res.json({ message: `Hello, ${name}!` });
+});
+
+module.exports = app;
